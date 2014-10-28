df43d9ca5e6fcf7ece8f0f14b3d3329597abd298
Jindrich Smitka <jsmitka@smita.info>
2014-10-28 12:07:22 +0100

    Added formatter test skeleton

---
+++ test/formatter_test.exs
@@ -0,0 +1,14 @@
+defmodule FormatterTest do
+  use ExUnit.Case, async: false
+
+  # test "to table return only selected columns" do
+  #   data = [
+  #     %{"foo" => 1, "bar" => 2, "baz" => 3},
+  #     %{"foo" => 4, "bar" => 5, "something" => 6},
+  #   ]
+
+  #   result = Issues.Formatter.to_table(data, ["foo", "bar"])
+
+  #   assert result == [[1,2], [4,5]]
+  # end
+end
