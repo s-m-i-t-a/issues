cc3555b2a5a4fa18ad3a5de519953ad11568eb50
Jindrich Smitka <jsmitka@smita.info>
2014-10-28 11:13:12 +0100

    Added select columns

--- lib/issues/formatter.ex
+++ lib/issues/formatter.ex
@@ -1,5 +1,15 @@
 defmodule Issues.Formatter do
   def print_table_for_columns(rows, headers) do
+    data = to_table(rows, headers)
 
+    IO.inspect(data)
+  end
+
+  defp to_table(rows, headers) do
+    for row <- rows do
+      for header <- headers do
+        row[header]
+      end
+    end
   end
 end
