5bd0bf46e94547c721282dde80b16d1d4b4c29bd
Jindrich Smitka <jsmitka@smita.info>
2014-10-29 22:09:20 +0100

    Changed compile log level

--- config/config.exs
+++ config/config.exs
@@ -5,6 +5,9 @@ use Mix.Config
 config :issues,
   github_url: "https://api.github.com"
 
+config :logger,
+  compile_time_purge_level: :info
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
